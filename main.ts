1624ed4f8b94374227c8b70c5ff7bc374476236c
angelicagrezende <angel.rezende@outlook.com>
2025-03-20 09:19:38 -0300

    commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+import { Personagem } from "./Class/Personagem";
+
+const person:Personagem = new Personagem(); //cria uma instância da classe Personagem
+
+person.nome = "Edecio";
+person.armadura = 104;
+person.ataque = 1;
+person.classe = "Padre";
+person.defesa = 1;
+person.intelecto = 1000;
+person.stamina= 99;
+person.vida = 5;
+person.raca = "Morto-Vivo";
+person.poderAtaque =10;;
+person.mana = 88;
